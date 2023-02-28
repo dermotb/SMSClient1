08b43315a53a103d882bee83d4a8fb3ff193492a
Dermot Boyle <dermotb@hotmail.com>
2023-02-28 19:39:26 +0000

    Add project files.

---
+++ SMSClient/Models/TextMessage.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SMS.Models
+{
+    public class TextMessage
+    {
+        [Required]
+        [Phone]
+        public string FromNumber { get; set; }
+
+        [Required]
+        [Phone]
+        public string ToNumber { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(140)]
+        public string Content { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("From: {0} | To: {1} | Message: {2}", FromNumber, ToNumber, Content);
+        }
+
+    }
+}
---
+++ SMSClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using SMS.Models;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace SMS
+{
+    public class Program
+    {
+        static void Main(string[] args) 
+        {
+            Task result = SendMessageAsync();
+            result.Wait();
+        }
+
+        private static async Task SendMessageAsync()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:5230/");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                TextMessage txt = new TextMessage() { FromNumber = "086-555444", ToNumber = "086-444555", Content = "Message from the client" };
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("api/SMS", txt);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Message sent");
+                }
+                else
+                {
+                    Console.WriteLine(response.StatusCode + " " + response.ReasonPhrase);
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
